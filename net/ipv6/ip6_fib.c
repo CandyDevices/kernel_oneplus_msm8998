6899075baa17401876ee16c8548815e6b3f16154
Ido Schimmel <idosch@mellanox.com>
2017-08-03 13:28:22 +0200

    UPSTREAM: ipv6: fib: Unlink replaced routes from their nodes
    When a route is deleted its node pointer is set to NULL to indicate it's
    no longer linked to its node. Do the same for routes that are replaced.
    
    This will later allow us to test if a route is still in the FIB by
    checking its node pointer instead of its reference count.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Cherry-pick from: 7483cea79957312e9f8e9cf760a1bc5d6c507113
    Bug: 64978549
    
    Change-Id: Ibfa54cf918084138b6b19437e9ef86bfaea5deae

--- net/ipv6/ip6_fib.c
+++ net/ipv6/ip6_fib.c
@@ -891,6 +891,7 @@ add:
 			fn->fn_flags |= RTN_RTINFO;
 		}
 		nsiblings = iter->rt6i_nsiblings;
+		iter->rt6i_node = NULL;
 		fib6_purge_rt(iter, fn, info->nl_net);
 		if (fn->rr_ptr == iter)
 			fn->rr_ptr = NULL;
@@ -905,6 +906,7 @@ add:
 					break;
 				if (rt6_qualify_for_ecmp(iter)) {
 					*ins = iter->dst.rt6_next;
+					iter->rt6i_node = NULL;
 					fib6_purge_rt(iter, fn, info->nl_net);
 					if (fn->rr_ptr == iter)
 						fn->rr_ptr = NULL;
